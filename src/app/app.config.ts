91d015173e5e6f4e7cc987e932ae75460f58a04d
Yasmine Ferjani <145272750+jassfbc@users.noreply.github.com>
2025-11-29 01:05:31 +0100

    failed toastr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,19 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimations(),
+    provideToastr()
+
   ]
+
 };
--- src/app/services/embauche-service.ts
+++ src/app/services/embauche-service.ts
@@ -1,18 +1,21 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Personne } from '../Model/Personne';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmbaucheService {
   liste_embauche: Personne[] = []; 
+  ToastrService = inject(ToastrService);
 
   embaucher(personne: Personne){
     if(this.liste_embauche.find((prs)=> prs.id === personne.id) == null){
         this.liste_embauche.push(personne);
     } 
     else{
-      alert(personne.name +' est déjà embauché!');
+      alert(personne.name + " est déjà embauché(e)");
+      // this.ToastrService.error(personne.name + " est déjà embauché(e)", "Action Répétée", {timeOut:3000});
     }
   }
   getListeEmbaucheFull(): Personne[]{
